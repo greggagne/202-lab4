13fba012fc81ad46110e60a0d9af4cbd9b18e995
greggagne <ggagne@westminsteru.edu>
2025-02-02 15:14:35 -0700

    Feb update.

---
+++ src/ArraySet.java
@@ -0,0 +1,42 @@
+public class ArraySet<T> implements Set<T> {
+
+    @Override
+    public void add(T element) {
+
+    }
+
+    @Override
+    public void addAll(T[] elements) {
+
+    }
+
+    @Override
+    public boolean contains(T element) {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void remove(T element) {
+
+    }
+
+    @Override
+    public Set<T> union(Set<T> anotherSet) {
+        return null;
+    }
+
+    @Override
+    public Set<T> intersection(Set<T> anotherSet) {
+        return null;
+    }
+
+    @Override
+    public Set<T> difference(Set<T> anotherSet) {
+        return null;
+    }
+}
